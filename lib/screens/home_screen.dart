62bfa154b60e46ffdc063d6bf72500b2d696540d
Jaka Kralj <jaka.kralj@gmail.com>
2023-05-07 23:58:06 +0200

    first commit

---
+++ lib/screens/home_screen.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import 'package:weather_src/utils/app_styles.dart';
+import 'package:weather_src/services/weather_api_service.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  WeatherApiService api = WeatherApiService();
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: AppTheme.dark,
+      body: Column(
+        children: [
+          ElevatedButton(
+            onPressed: () async {
+              final response = await api.fetchWeatherData();
+              print(response);
+            },
+            child: const Text('Data'),
+          ),
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/services/weather_api_service.dart
@@ -0,0 +1,18 @@
+import 'package:http/http.dart' as http;
+import 'package:charts_flutter/flutter.dart' as charts;
+import 'dart:convert';
+import 'package:flutter/material.dart';
+import 'package:geocoding/geocoding.dart';
+
+class WeatherApiService {
+  Future<Map<String, dynamic>> fetchWeatherData() async {
+    const String apiUrl =
+        "https://api.open-meteo.com/v1/forecast?latitude=46.05&longitude=14.51&hourly=temperature_2m";
+    final response = await http.get(Uri.parse(apiUrl));
+    if (response.statusCode == 200) {
+      return jsonDecode(response.body);
+    } else {
+      throw Exception('Failed to load weather data');
+    }
+  }
+}
