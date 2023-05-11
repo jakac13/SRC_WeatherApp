f092eddfaaf5bf9e3977c04001fb516fd1ef74e7
Jaka Kralj <jaka.kralj@gmail.com>
2023-05-11 14:50:13 +0200

    Added extra title for 7 day forecast

--- lib/components/weather_forecast_component.dart
+++ lib/components/weather_forecast_component.dart
@@ -28,80 +28,94 @@ class _WeatherForecastComponentState extends State<WeatherForecastComponent> {
       return Expanded(
         child: SingleChildScrollView(
           child: Column(
-            children: List.generate(
-              viewModel.weatherData!.daily!.time!.length,
-              (index) {
-                DateTime date = DateTime.parse(dailyWeather.time![index]);
-                String formattedDate = DateFormat('EEEE').format(date);
-                return Container(
-                  child: Card(
-                    shape: const RoundedRectangleBorder(
-                        borderRadius: BorderRadius.all(Radius.circular(15))),
-                    child: ClipRRect(
-                      borderRadius: BorderRadius.circular(15),
-                      child: Container(
-                        padding: const EdgeInsets.symmetric(
-                            vertical: 20, horizontal: 20),
-                        child: Row(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          children: [
-                            Image.asset(
-                              weatherImageProvider
-                                  .getWeatherImage(
-                                      dailyWeather.weathercode![index])
-                                  .toString(),
-                            ),
-                            const SizedBox(
-                              width: 30,
-                            ),
-                            Column(
-                              crossAxisAlignment: CrossAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              const Text(
+                "Weather 7 day forecast",
+                style: TextStyle(fontSize: 32, fontWeight: FontWeight.bold),
+              ),
+              const SizedBox(
+                height: 10,
+              ),
+              Column(
+                children: List.generate(
+                  viewModel.weatherData!.daily!.time!.length,
+                  (index) {
+                    DateTime date = DateTime.parse(dailyWeather.time![index]);
+                    String formattedDate = DateFormat('EEEE').format(date);
+                    return Container(
+                      child: Card(
+                        shape: const RoundedRectangleBorder(
+                            borderRadius:
+                                BorderRadius.all(Radius.circular(15))),
+                        child: ClipRRect(
+                          borderRadius: BorderRadius.circular(15),
+                          child: Container(
+                            padding: const EdgeInsets.symmetric(
+                                vertical: 20, horizontal: 20),
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.center,
                               children: [
-                                Text(
-                                  formattedDate,
-                                  style: const TextStyle(
-                                      fontSize: 16,
-                                      fontWeight: FontWeight.bold),
+                                Image.asset(
+                                  weatherImageProvider
+                                      .getWeatherImage(
+                                          dailyWeather.weathercode![index])
+                                      .toString(),
                                 ),
                                 const SizedBox(
-                                  height: 10,
+                                  width: 30,
                                 ),
-                                Row(
-                                  mainAxisAlignment: MainAxisAlignment.start,
+                                Column(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
                                   children: [
-                                    const Icon(
-                                      Icons.thermostat,
-                                      size: 20,
-                                      color: Colors.blueAccent,
+                                    Text(
+                                      formattedDate,
+                                      style: const TextStyle(
+                                          fontSize: 16,
+                                          fontWeight: FontWeight.bold),
                                     ),
                                     const SizedBox(
-                                      width: 10,
+                                      height: 10,
                                     ),
-                                    Text(
-                                      '${dailyWeather.temperature2mMax![index]}  |',
-                                      style: const TextStyle(
-                                          fontWeight: FontWeight.bold,
-                                          color: AppTheme.dark_light),
-                                    ),
-                                    Text(
-                                      '  ${dailyWeather.temperature2mMin![index]}',
-                                      style: const TextStyle(
-                                        color: AppTheme.dark_light,
-                                        fontWeight: FontWeight.bold,
-                                      ),
+                                    Row(
+                                      mainAxisAlignment:
+                                          MainAxisAlignment.start,
+                                      children: [
+                                        const Icon(
+                                          Icons.thermostat,
+                                          size: 20,
+                                          color: Colors.blueAccent,
+                                        ),
+                                        const SizedBox(
+                                          width: 10,
+                                        ),
+                                        Text(
+                                          '${dailyWeather.temperature2mMax![index]}  |',
+                                          style: const TextStyle(
+                                              fontWeight: FontWeight.bold,
+                                              color: AppTheme.dark_light),
+                                        ),
+                                        Text(
+                                          '  ${dailyWeather.temperature2mMin![index]}',
+                                          style: const TextStyle(
+                                            color: AppTheme.dark_light,
+                                            fontWeight: FontWeight.bold,
+                                          ),
+                                        ),
+                                      ],
                                     ),
                                   ],
-                                ),
+                                )
                               ],
-                            )
-                          ],
+                            ),
+                          ),
                         ),
                       ),
-                    ),
-                  ),
-                );
-              },
-            ),
+                    );
+                  },
+                ),
+              ),
+            ],
           ),
         ),
       );
